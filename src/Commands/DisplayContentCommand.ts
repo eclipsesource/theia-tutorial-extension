87b4c74ad52ac9b723d74af198ebf7a94148feba
lukasBoll <ga24xeg@mytum.de>
2020-11-24 20:15:58 +0100

    impl chack state

--- src/Commands/DisplayContentCommand.ts
+++ src/Commands/DisplayContentCommand.ts
@@ -1,63 +1,66 @@
 import * as vscode from 'vscode';
+const fs = require('fs');
+const path = require('path');
 
 const getDisplayContentCommand = (context: vscode.ExtensionContext): vscode.Disposable => {
 
   const DISPLAYCONTENTCOMMAND: vscode.Disposable = vscode.commands.registerCommand('theiatutorialextension.displayContent', () => {
+    const workspaceFolder: string = vscode.workspace.rootPath || '~';
 
 
     vscode.commands.executeCommand('vscode.setEditorLayout', {
       orientation: 0,
-      groups: [{ size: 0.6 }, { size: 0.4 }],
-      })
+      groups: [{size: 0.6}, {size: 0.4}],
+    })
 
     const panel = vscode.window.createWebviewPanel(
-        'react', // Identifies the type of the webview. Used internally
-        'Theia Tutorial', // Title of the panel displayed to the user
-        vscode.ViewColumn.Two, // Editor column to show the new webview panel in.
-        {
-            enableScripts: true,
-            retainContextWhenHidden: true,
-        } // Webview options. More on these later.
-      );
-      let nextStep = false;
-      let isStateChecked = false;
+      'react', // Identifies the type of the webview. Used internally
+      'Theia Tutorial', // Title of the panel displayed to the user
+      vscode.ViewColumn.Two, // Editor column to show the new webview panel in.
+      {
+        enableScripts: true,
+        retainContextWhenHidden: true,
+      } // Webview options. More on these later.
+    );
+    let nextStep = false;
+    let isStateChecked = false;
 
-          // And set its HTML content
-          panel.webview.html = getWebviewContent(nextStep, isStateChecked);
+    // And set its HTML content
+    panel.webview.html = getWebviewContent(nextStep, isStateChecked);
 
-      // Handle messages from the webview
-      panel.webview.onDidReceiveMessage(
-        message => {
-          switch (message.command) {
-            case 'nextStep':
-              vscode.window.showInformationMessage('Next Step')
+    // Handle messages from the webview
+    panel.webview.onDidReceiveMessage(
+      message => {
+        switch (message.command) {
+          case 'nextStep':
+            vscode.window.showInformationMessage('Next Step')
 
-              nextStep = message.value;
+            nextStep = message.value;
+            panel.webview.html = getWebviewContent(nextStep, isStateChecked);
+            return;
+
+          case 'checkState':
+
+            if (message.value) {
+              checkCurrentState();
               panel.webview.html = getWebviewContent(nextStep, isStateChecked);
-              return;
-
-            case 'checkState':
-
-              if(message.value){
-                isStateChecked = message.value;
-                checkCurrentState();
-                panel.webview.html = getWebviewContent(nextStep, isStateChecked);
-              }
-              return;
-          }
-        },
-        undefined,
-        context.subscriptions
-      );
+            }
+            return;
+        }
+      },
+      undefined,
+      context.subscriptions
+    );
 
 
-      const checkCurrentState = () => {
-        vscode.window.showInformationMessage('Current State is checked')
-      }
+    const checkCurrentState = () => {
+      isStateChecked = fs.existsSync(path.join(workspaceFolder, ".tutorialRepo"));
+      vscode.window.showInformationMessage('Current State is checked')
+    }
 
-});
+  });
 
-return DISPLAYCONTENTCOMMAND;
+  return DISPLAYCONTENTCOMMAND;
 }
 
 
@@ -65,8 +68,8 @@ return DISPLAYCONTENTCOMMAND;
 function getWebviewContent(nextStep: Boolean, isStateChecked: Boolean) {
 
 
-  return !nextStep ? 
-`<!DOCTYPE html>
+  return !nextStep ?
+    `<!DOCTYPE html>
   <html lang="en">
   <head>
 	  <meta charset="UTF-8">
@@ -77,7 +80,7 @@ function getWebviewContent(nextStep: Boolean, isStateChecked: Boolean) {
 	<div style="display: flex; flex-direction: column">
 		<img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
     <button style="margin: 20px; width: fit-content" onclick="checkState()">Check the current state</button> 
-    <button id="stateButton" style="margin: 20px; width: fit-content" onclick="goToNextStep()" disabled>Next Step</button> 
+    <button id="stateButton" style="margin: 20px; width: fit-content" onclick="goToNextStep()" ` + (isStateChecked ? `` : `disabled`) + `>Next Step</button> 
   </div>
   
   <script>
@@ -99,17 +102,15 @@ function getWebviewContent(nextStep: Boolean, isStateChecked: Boolean) {
       value: true
   })
 
-  document.getElementById("stateButton").disabled = false; 
-
 }
 
   </script>
   </body>
   </html>`
 
-  :
-  
-  `<!DOCTYPE html>
+    :
+
+    `<!DOCTYPE html>
   <html lang="en">
   <head>
 	  <meta charset="UTF-8">
@@ -123,7 +124,7 @@ function getWebviewContent(nextStep: Boolean, isStateChecked: Boolean) {
   </div>
   </body>
   </html>`;
-  }
+}
 
 
 export default getDisplayContentCommand;
