b9b2aa246c363b645ad6bd61e3c5d3ef7be4e162
Max Elia <max_elia@hotmail.de>
2020-12-01 20:19:28 +0100

    Displaycommand

--- src/initCommands.ts
+++ src/initCommands.ts
@@ -2,16 +2,11 @@ import * as vscode from 'vscode';
 import CLONEREPOCOMMAND from './Commands/CloneRepoCommand';
 import EXECUTETESTSCOMMAND from './Commands/ExecuteTests';
 import INITEXERCISEZEROCOMMAND from './Commands/InitExerciseZero';
-import DISPLAYCONTENTCOMMAND from './Commands/DisplayContentCommand';
-
-import getDisplayContentCommand from './Commands/DisplayContentCommand';
 
 function initCommands (context: vscode.ExtensionContext) {
-	// const DISPLAYCONTENTCOMMAND = getDisplayContentCommand(context)
 
 	const commands = [
 		CLONEREPOCOMMAND,
-		DISPLAYCONTENTCOMMAND,
 		EXECUTETESTSCOMMAND,
 		INITEXERCISEZEROCOMMAND,
 		EXECUTETESTSCOMMAND
