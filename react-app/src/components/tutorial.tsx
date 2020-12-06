922a901c4512796750b82ba935e27e762c88708b
Max Elia <max_elia@hotmail.de>
2020-12-06 11:56:13 +0100

    Load Exercise content from JSON

--- react-app/src/components/tutorial.tsx
+++ react-app/src/components/tutorial.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Button} from '@material-ui/core';
 import {VSCodeAPI} from '../VSCodeAPI';
+import { Exercise } from './exercise';
 
 
 interface TutorialProps {
@@ -16,17 +17,6 @@ export class Tutorial extends React.Component<TutorialProps, TutorialState> {
 
     constructor(props: TutorialProps) {
         super(props);
-        this.state = {
-            info: 'You can get ready now. Prepare the workspace by yourself or let us do the work for you.',
-        }
-        VSCodeAPI.onMessage((message) => {
-            console.log(message);
-            switch (message.data.command) {
-                case 'setInfo':
-                    this.setState({info: message.data.text});
-                    break;
-            }
-        });
     }
     render() {
         return (
@@ -34,20 +24,8 @@ export class Tutorial extends React.Component<TutorialProps, TutorialState> {
                 <header className="App-header">
                     <h1 className="App-title">{this.props.tutorial.title}</h1>
                 </header>
-                <p className="App-intro">
-                    In this tutorial we are going to implement a Theia extension.
-          </p>
-                <p>{this.state.info}</p>
-                <div className="Box-margin">
-                    <Button onClick={() => VSCodeAPI.postMessage({command: 'checkExerciseFiles'})} variant="contained" color="primary">
-                        Check workspace
-            </Button>
-                </div>
-                <div className="Box-margin">
-                    <Button onClick={() => VSCodeAPI.postMessage({command: 'initExerciseZero'})} variant="contained" color="primary">
-                        Solve Exercise 0
-            </Button>
-                </div>
+                
+                <Exercise exercise={this.props.tutorial.tutorial[0]}></Exercise>
             </div>
         );
     }
