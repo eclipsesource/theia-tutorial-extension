2173115f25f40fd32e3a710f9d05c0f31e18a3a6
buraktomruk <btburak@hotmail.com>
2020-12-17 12:15:24 +0100

    update command util

--- src/utils/commandUtil.ts
+++ src/utils/commandUtil.ts
@@ -1,6 +1,12 @@
 import { exec } from 'child_process';
+import * as vscode from 'vscode';
+
+const workspaceFolder: string = vscode.workspace.rootPath || '~';
 
 export async function execShellCommand(cmd: string) {
+
+    process.chdir(workspaceFolder);
+
     return new Promise((resolve, reject) => {
         exec(cmd, (error, stdout, stderr) => {
             if (error) {
