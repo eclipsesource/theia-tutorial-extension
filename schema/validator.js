7c20173ad399cccb7ef3565982c2e8c930079f28
lukasBoll <ga24xeg@mytum.de>
2021-01-06 03:33:31 +0100

    json schema

---
+++ schema/validator.js
@@ -0,0 +1,10 @@
+// Node.js require:
+const Ajv = require("ajv").default
+
+const schema = require('./tutorial.json');
+const data = require('../tutorials/tutorial0.tut.json');
+
+const ajv = new Ajv() // options can be passed, e.g. {allErrors: true}
+const validate = ajv.compile(schema)
+const valid = validate(data)
+if (!valid) console.log(validate.errors)
