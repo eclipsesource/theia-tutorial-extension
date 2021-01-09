0d4d5df069ff150900664df14aff895023fa1e56
lukasBoll <ga24xeg@mytum.de>
2021-01-09 14:01:03 +0100

    added timestamp

--- src/Functions/moveExerciseFolder.ts
+++ src/Functions/moveExerciseFolder.ts
@@ -3,6 +3,7 @@ import * as vscode from 'vscode';
 
 export function moveEcerciseFolder(exerciseFolder: string): void {
     const workspaceFolder: string = vscode.workspace.rootPath || '~';
+    let today = new Date();
     let currentTimeStamp = today.getFullYear() + '_' + (today.getMonth() + 1) + '_' + today.getDate() + '_' + today.getHours() + "_" + today.getMinutes() + "_" + today.getSeconds();;
     exec("move " + workspaceFolder + "/" + exerciseFolder + " " + workspaceFolder + "/.tmp");
 }
