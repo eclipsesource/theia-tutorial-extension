58adb6fa84a3ed1d21f17c246a1f863cd53dfc27
buraktomruk <buraktomruk95@gmail.com>
2021-02-20 11:39:07 +0100

    update tsconfig and filepicker

--- tree-editor-extension/tree-editor-extension/src/browser/tree/FilePickerControl.tsx
+++ tree-editor-extension/tree-editor-extension/src/browser/tree/FilePickerControl.tsx
@@ -1,7 +1,8 @@
-import * as React from "react";
-import { withJsonFormsControlProps } from "@jsonforms/react";
-import { FilePicker } from "./FilePicker";
-import { rankWith, scopeEndsWith } from "@jsonforms/core";
+/* eslint-disable @typescript-eslint/naming-convention */
+import * as React from 'react';
+import { withJsonFormsControlProps } from '@jsonforms/react';
+import { FilePicker } from './FilePicker';
+import { rankWith, scopeEndsWith, OwnPropsOfControl } from '@jsonforms/core';
 
 interface FilePickerControlProps {
   data: any;
@@ -11,48 +12,39 @@ interface FilePickerControlProps {
 
 export const imageTester = rankWith(
   1000, //increase rank as needed
-  scopeEndsWith("src")
+  scopeEndsWith('src')
 );
 
 export const htmlTester = rankWith(
   1000, //increase rank as needed
-  scopeEndsWith("html")
+  scopeEndsWith('html')
 );
 
 export const filePickerControlTester = rankWith(
   1000, //increase rank as needed
-  scopeEndsWith("openFile")
+  scopeEndsWith('openFile')
 );
 
-export const FilePickerControl = withJsonFormsControlProps(
+export const FilePickerControl: React.ComponentType<OwnPropsOfControl> = withJsonFormsControlProps(
   ({ data, handleChange, path }: FilePickerControlProps) => (
     <FilePicker
       value={data}
       updateValue={(newValue: string) => handleChange(path, newValue)}
-      inputAcceptType=".ts,.js"
-      label="Upload Code (.js or .ts)"
+      inputAcceptType='.ts,.js'
+      label='Upload Code (.js or .ts)'
     />
   )
 );
 
-export const ImagePickerControl = withJsonFormsControlProps(
+export const ImagePickerControl: React.ComponentType<OwnPropsOfControl> = withJsonFormsControlProps(
   ({ data, handleChange, path }: FilePickerControlProps) => (
     <FilePicker
       value={data}
-      updateValue={(newValue: string) => handleChange(path, ".tutorial/assets/"+newValue)}
-      inputAcceptType="image/*"
-      label="Upload Image File"
-    />
-  )
-);
-
-export const HtmlPickerControl = withJsonFormsControlProps(
-  ({ data, handleChange, path }: FilePickerControlProps) => (
-    <FilePicker
-      value={data}
-      updateValue={(newValue: string) => handleChange(path, ".tutorial/html/"+newValue)}
-      inputAcceptType=".html"
-      label="Upload Html File"
+      updateValue={(newValue: string) =>
+        handleChange(path, '.tutorial/assets/' + newValue)
+      }
+      inputAcceptType='image/*'
+      label='Upload Image File'
     />
   )
 );
